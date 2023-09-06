8b391d9e023bda0c71996aae3f6d58c09635d062
Jordan Montano Núñez <jmontanonunez@gmail.com>
2023-09-06 19:48:12 -0300

    Add service to return non terminated contracts

--- src/routes/contracts.js
+++ src/routes/contracts.js
@@ -1,10 +1,11 @@
 const { getProfile } = require('../middleware/getProfile')
+const ContractStatus = require('../enums/contract_status')
 const { Op } = require('sequelize')
 const express = require('express')
 const router = express.Router()
 
 /**
- * Search for a contract by id that belongs to the profile that's querying.
+ * Seeks for a contract by id that belongs to the profile that's requesting.
  * @returns contract by id.
  */
 router.get('/:id', getProfile, async (req, res) => {
@@ -24,6 +25,36 @@ router.get('/:id', getProfile, async (req, res) => {
     }
   }
   const contract = await Contract.findOne(query)
+
+  if (!contract) return res.status(404).end()
+
+  res.json(contract)
+})
+
+/**
+ * Seeks for non terminated contracts belongs to the profile that's requesting.
+ * @returns a list of contracts
+ */
+router.get('/', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models')
+  const query = {
+    where: {
+      [Op.and]: [
+        {
+          status: {
+            [Op.in]: [ContractStatus.New, ContractStatus.InProgress]
+          }
+        },
+        {
+          [Op.or]: [
+            { ContractorId: req.profile.id },
+            { ClientId: req.profile.id }
+          ]
+        }
+      ]
+    }
+  }
+  const contract = await Contract.findAll(query)
 
   if (!contract) return res.status(404).end()
 
