ffcb58e0d3d3380bf1ce68bdb34cea46f56ae8c0
Jordan Montano Núñez <jmontanonunez@gmail.com>
2023-09-06 19:11:50 -0300

    Refactor sequelize models

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { sequelize } = require('./model')
+const express = require('express')
+const bodyParser = require('body-parser')
+const sequelize = require('./sequelize')
 const { getProfile } = require('./middleware/getProfile')
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+
+// Import sequelize models
+const Profile = require('./models/profile')
+const Job = require('./models/job')
+const Contract = require('./models/contract')
+
+// Configure relationships
+Profile.hasMany(Contract, { as: 'Contractor', foreignKey: 'ContractorId' })
+Profile.hasMany(Contract, { as: 'Client', foreignKey: 'ClientId' })
+Contract.belongsTo(Profile, { as: 'Contractor' })
+Contract.belongsTo(Profile, { as: 'Client' })
+Contract.hasMany(Job)
+Job.belongsTo(Contract)
+
+// Config express
+app.use(bodyParser.json())
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
@@ -12,10 +27,10 @@ app.set('models', sequelize.models)
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
-    const { Contract } = req.app.get('models')
-    const { id } = req.params
-    const contract = await Contract.findOne({ where: { id } })
-    if (!contract) return res.status(404).end()
-    res.json(contract)
+  const { Contract } = req.app.get('models')
+  const { id } = req.params
+  const contract = await Contract.findOne({ where: { id } })
+  if (!contract) return res.status(404).end()
+  res.json(contract)
 })
-module.exports = app;
+module.exports = app
