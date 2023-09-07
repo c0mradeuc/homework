39109fffdc52090fd095776157cb8908a668b722
Jordan Montano Núñez <jmontanonunez@gmail.com>
2023-09-07 20:23:50 -0300

    Implemented validate model middleware

---
+++ src/middleware/validateModel.js
@@ -0,0 +1,16 @@
+const ModelValidationError = require('../errorHandling/modelValidationError')
+
+const validateModel = function(schema) {
+  return (req, res, next) => {
+    const model = { ...req.body, ...req.query }
+    const { error } = schema.validate(model)
+
+    if (error) {
+      throw new ModelValidationError(error.details)
+    }
+
+    next()
+  }
+}
+
+module.exports = validateModel
