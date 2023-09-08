756efe718d2b8cef4419c576c7f09a9c03134281
Jordan Montano Núñez <jmontanonunez@gmail.com>
2023-09-07 22:24:31 -0300

    Fix logging option

--- src/sequelize.js
+++ src/sequelize.js
@@ -5,7 +5,7 @@ const isTesting = process.env.NODE_ENV === 'test'
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: isTesting ? ':memory:' : './database.sqlite3',
-  logging: !isTesting
+  logging: isTesting ? false : console.log
 })
 
 module.exports = sequelize
